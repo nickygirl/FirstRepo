a3dff076dc4953ffbfc35c87a65b6d9a7b51aaec
nickygirl <531374972@qq.com>
2016-09-16 20:12:33 +0800

    new an empty project.

---
+++ FirstRepo/FirstRepo.cpp
@@ -0,0 +1,12 @@
+// FirstRepo.cpp : 定义控制台应用程序的入口点。
+//
+#include <iostream>
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "this is the first repo!";
+	return 0;
+}
+
